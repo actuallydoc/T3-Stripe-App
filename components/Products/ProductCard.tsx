bc27082894e0e8f59ce555821e5e6129fe7e27b2
Maj Knez <actuallydoc@gmail.com>
2023-05-29 11:56:39 +0200

    If you click anything in the box it redirects

--- components/Products/ProductCard.tsx
+++ components/Products/ProductCard.tsx
@@ -12,31 +12,35 @@ export default function ProductCard({ item, handleAddToCart }: { item: Stripe.Pr
     return (
         <div>
             {item.active ? (
-                <div key={item.id} className='box-content w-auto h-auto hover:translate-x-2 duration-500'>
-                    <div key={item.id} onClick={() => {
+                <div key={item.id} className='box-content w-auto h-auto hover:scale-105 duration-500 cursor-pointer'>
+                    <div onClick={() => {
                         handleProductClick().then((r) => console.log(r)).catch((e) => console.log(e));
-                    }} className='flex flex-col items-center justify-center w-auto h-auto p-4 bg-white rounded-lg shadow-lg'>
-                        <div>
-                            {item.images[0] ? (
-                                <Image src={item.images[0]} className='w-auto h-auto' alt="product-image" width={200} height={200} />
-                            ) : null}
+                    }}>                    <div key={item.id}
+                        className='flex flex-col items-center justify-center w-auto h-auto p-4 bg-white rounded-lg shadow-lg'>
+                            <div>
+                                {item.images[0] ? (
+                                    <Image src={item.images[0]} className='w-auto h-auto' alt="product-image" width={200} height={200} />
+                                ) : null}
+                            </div>
+                            <div>
+                                <h1 className='text-xl font-bold text-gray-900'>{item.name}</h1>
+                            </div>
+                            <div>
+                                <h1 className='text-sm font-semibold text-gray-900'>{item.description}</h1>
+                            </div>
                         </div>
-                        <div>
-                            <h1 className='text-xl font-bold text-gray-900'>{item.name}</h1>
-                        </div>
-                        <div>
-                            <h1 className='text-sm font-semibold text-gray-900'>{item.description}</h1>
-                        </div>
+
+                    </div>
+                    <div>
                         <div>
                             <button onClick={() => {
                                 handleAddToCart(item.default_price as string);
                             }} className='mt-4 bg-sky-700 text-white px-3 py-2 rounded-full hover:bg-sky-500'>Add to cart</button>
                         </div>
-
                     </div>
                 </div>
             ) : (
-                <div key={item.id} className='box-content w-auto h-auto hover:translate-x-2 duration-500'>
+                <div key={item.id} className='box-content w-auto h-auto hover:scale-105 duration-300'>
                     <div key={item.id} className='flex flex-col items-center justify-center w-auto h-auto p-4 bg-white rounded-lg shadow-lg'>
                         <div>
                             {item.images[0] ? (
@@ -49,14 +53,18 @@ export default function ProductCard({ item, handleAddToCart }: { item: Stripe.Pr
                         <div>
                             <h1 className='text-sm font-semibold text-gray-900'>{item.description}</h1>
                         </div>
+
+                    </div>
+                    <div>
                         <div>
                             <button onClick={() => {
                                 handleAddToCart(item.default_price as string);
-                            }} className='mt-4 bg-slate-400 px-3 py-2 rounded-full' disabled>Out of stock</button>
+                            }} className='mt-4 bg-sky-700 text-white px-3 py-2 rounded-full disabled opacity-60'>Out of stock</button>
                         </div>
                     </div>
                 </div>
-            )}
-        </div>
+            )
+            }
+        </div >
     )
 }
