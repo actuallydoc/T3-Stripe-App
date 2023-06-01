449a0e3036179c226b097458504467c788b1bad3
Maj Knez <actuallydoc@gmail.com>
2023-06-01 22:09:14 +0200

    Custom 404 page with a redirect back

---
+++ src/pages/404.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
+import toast, { Toaster } from 'react-hot-toast';
+
+export default function Custom404() {
+    const router = useRouter();
+    //Just instantly redirect the user to the home page
+    useEffect(() => {
+        toast.error("Page not found");
+        setTimeout(() => {
+            router.push("/").catch((err) => {
+                console.log(err);
+            });
+        }
+            , 4000);
+
+    }, [router]);
+    return (
+        <div>404
+            <Toaster />
+        </div>
+    )
+}
